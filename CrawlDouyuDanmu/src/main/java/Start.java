d533c7809198ecc778ae55bf98d9e55a0a236c32
sun.sun <Sun.Sun@netis.com>
2019-09-12 18:00:16 +0800

    douyu弹幕数据持久化，可做分析

--- CrawlDouyuDanmu/src/main/java/Start.java
+++ CrawlDouyuDanmu/src/main/java/Start.java
@@ -1,11 +1,16 @@
+import mysql.Main;
+
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class Start {
     public static void main(String[] args) throws IOException, InterruptedException {
         DouyuBarrageHandler server = new DouyuBarrageHandler("9999");//房间号id
         while (true) {
-            System.out.println(server.read());
-            Thread.sleep(1);
+            String temp=server.read();
+            System.out.println(temp);
+            Main.insert(temp);
+           // Thread.sleep(1);
         }
     }
 }
